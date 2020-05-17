3bc02243d6c8aa66ac0593324f660bfbf6a0f066
HervalMata <hervalmataamparo@gmail.com>
2020-05-17 15:43:23 -0300

    Ajustes

--- LojaVirtual/Controllers/Home.cs
+++ LojaVirtual/Controllers/Home.cs
@@ -4,35 +4,35 @@ namespace LojaVirtual.Controllers
 {
     public class Home : Controller
     {
-        public IActionResult index()
+        public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult contato()
+        public IActionResult Contato()
         {
             return View();
         }
 
-        public IActionResult contatoAcao()
+        public IActionResult ContatoAcao()
         {
             string nome = HttpContext.Request.Form["nome"];
             string email = HttpContext.Request.Form["email"];
             string texto = HttpContext.Request.Form["texto"];
-            return new ContentResult() {Content = string.Format("dados recebidos com sucesso!<br/> Nome: (0) <br/> E-mail: (1) <br/> Texto: (2)", nome, email, texto)};
+            return new ContentResult() {Content = string.Format("dados recebidos com sucesso!<br/> Nome: {0} <br/> E-mail: {1} <br/> Texto: {2}", nome, email, texto), ContentType = "text/html"};
         }
 
-        public IActionResult login()
+        public IActionResult Login()
         {
             return View();
         }
 
-        public IActionResult cadastroCliente()
+        public IActionResult CadastroCliente()
         {
             return View();
         }
 
-        public IActionResult carrinhoCompras()
+        public IActionResult CarrinhoCompras()
         {
             return View();
         }
